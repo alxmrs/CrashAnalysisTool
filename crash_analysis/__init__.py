a064ed3c362022eb45f2081c8dedac4f20812465
Alex Rosengarten <alexrosengarten@gmail.com>
2017-04-14 16:16:30 -0700

    - Updated project to python 3.5 - Fixed xml to dataframe parser - parallelized downloader - Created ExampleNotebook - Updated demo and renamed to CrashAnalysisTour

--- crash_analysis/__init__.py
+++ crash_analysis/__init__.py
@@ -6,3 +6,6 @@ from .dataframe_helper import *
 
 from .private import QUICKBASE as QB
 from .quickbase import Client
+
+from . import downloader
+
--- crash_analysis/downloader.py
+++ crash_analysis/downloader.py
@@ -1,16 +1,20 @@
 from crash_analysis import QB
 from crash_analysis import Client
+from itertools import repeat
+from multiprocessing import Pool
 
+import multiprocessing
 import datetime
 import urllib.request as req
-
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-default_dest = os.path.join(BASE_DIR, 'tmp/')
+default_dest = os.path.join(BASE_DIR, 'tmp' + os.sep)
 
 DEBUG = True
+
+optimal_thread_count = multiprocessing.cpu_count() + 1
 
 def test_query():
 
@@ -23,17 +27,25 @@ def test_query():
     if(DEBUG):
        print('\n\n\nFinished.')
 
+def download_time_range(start, end, dest=None):
+    """
 
-def download_time_range(start, end):
+    Example:
+    >>> import datetime
+    >>> now = datetime.datetime.now()
+    >>> start = now + datetime.timedelta(hours=-1)
+    >>> download_time_range(start, end)
+    """
     client = Client(**QB)
     records = client.do_query("{'1'.GTE.'%s'}AND{'1'.LT.'%s'}" % (start.isoformat(), end.isoformat()),
                               structured=True, include_rids=True, columns=[14], path_or_tag='./table/records/record/f/url'
-)
+                             )
 
-    for record in records:
-        download_file(record['url'], client.ticket)
+    if (DEBUG):
+        print('About to download {0} records.'.format(len(records)))
 
-
+    with Pool(processes=optimal_thread_count) as pool:
+        pool.starmap(download_file, zip(map(lambda r: r['url'], records), repeat(client.ticket), repeat(dest)))
 
 
 def download_file(url, ticket, dest=None):
@@ -51,4 +63,6 @@ def download_file(url, ticket, dest=None):
 
 
 if __name__ == '__main__':
+    # import doctest
+    # doctest.testmod()
     test_query()
