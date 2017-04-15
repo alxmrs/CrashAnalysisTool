f420aefc07f944b707ccd41e4fbaa8fb21cab382
Alex Rosengarten <alexrosengarten@gmail.com>
2017-04-14 18:11:19 -0700

    Got rid of messy jupyter notebooks, csv files and py files that have since been ignored

--- src/test.py
+++
@@ -1,9 +0,0 @@
-from crash_analysis.parser import *
-
-import os
-def main():
-    zip_loc = './tmp/'
-    extract_zipfiles(zip_loc)
-    # df = xmldocs_to_dataframe(zip_loc)
-if __name__ == '__main__':
-    main()
