911f8f7cd96e144ce38ed807b7f13ae3a50d0da0
arosengarten <alex_rosengarten@gmail.com>
2017-10-25 20:44:38 -0700

    (Doctests and Static Typing)
    - All methods in df_helper are typed and tested (maybe not all paths)
    - Many methods in `preprocess.py` are typed and tested.

--- crash_analysis/dataframe_helper.py
+++ crash_analysis/dataframe_helper.py
@@ -1,39 +1,109 @@
 import os
 
 import pandas as pd
+from typing import List, Any
 
 
-def remove_empty(df):
-    """Remove rows with any empty (NaN/None) values"""
+def remove_empty(df: pd.DataFrame):
+    """Remove rows with any empty (NaN/None) values
+    
+    >>> df = pd.DataFrame({'A': list(range(0, 10)), 'B': list(range(10,20))})
+    >>> df['A'][1] = None
+    >>> df
+         A   B
+    0  0.0  10
+    1  NaN  11
+    2  2.0  12
+    3  3.0  13
+    4  4.0  14
+    5  5.0  15
+    6  6.0  16
+    7  7.0  17
+    8  8.0  18
+    9  9.0  19
+    >>> remove_empty(df)
+         A   B
+    0  0.0  10
+    2  2.0  12
+    3  3.0  13
+    4  4.0  14
+    5  5.0  15
+    6  6.0  16
+    7  7.0  17
+    8  8.0  18
+    9  9.0  19
+    """
     return df.dropna(how='any')
 
 
-def fill_empty(df):
-    """Fill rows with empty (NaN/None) values with ' '"""
-    return df.fillna(value=' ')
-
-
-def is_df_set(df):
+def fill_empty(df: pd.DataFrame, val: Any = ' ') -> pd.DataFrame:
+    """Fill rows with empty (NaN/None) values with ' '
+    
+    >>> df = pd.DataFrame({'A': list(range(0, 10)), 'B': list(range(10,20))})
+    >>> df['A'][1] = None
+    >>> df
+         A   B
+    0  0.0  10
+    1  NaN  11
+    2  2.0  12
+    3  3.0  13
+    4  4.0  14
+    5  5.0  15
+    6  6.0  16
+    7  7.0  17
+    8  8.0  18
+    9  9.0  19
+    >>> fill_empty(df, 0)
+         A   B
+    0  0.0  10
+    1  0.0  11
+    2  2.0  12
+    3  3.0  13
+    4  4.0  14
+    5  5.0  15
+    6  6.0  16
+    7  7.0  17
+    8  8.0  18
+    9  9.0  19
     """
-    Test to see if variable is None or has a dataframe object
+    return df.fillna(value=val)
+
+
+def is_df_set(df: pd.DataFrame) -> bool:
+    """Test to see if variable is None or has a dataframe object
+    
     :param df: dataframe to test
     :return: boolean
+    
+    >>> df = pd.DataFrame({'A': list(range(0, 10)), 'B': list(range(10,20))})
+    >>> is_df_set(df)
+    True
+    >>> is_df_set(None)
+    False
     """
     # type(df) is not type(None)  # old version
-    return isinstance(df, type(None))
+    return not isinstance(df, type(None))
 
 
-def get_columns(df):
-    """
-    Returns list of column titles for input or object dataframe.
+def get_columns(df: pd.DataFrame) -> List[str]:
+    """ Returns list of column titles for input or object dataframe.
+    
     :param df: dataframe
     :return: list of strings -- titles of dataframe columns
+    
+    >>> get_columns(list(range(10)))
+    Traceback (most recent call last):
+    ...
+    AssertionError
+    >>> df = pd.DataFrame({'A': list(range(0, 10)), 'B': list(range(10,20))})
+    >>> get_columns(df)
+    ['A', 'B']
     """
     assert isinstance(df, pd.DataFrame)
     return [c for c in df.columns]
 
 
-def read_csv(csv_file_path):
+def read_csv(csv_file_path: str) -> pd.DataFrame:
     """
     Reads a CSV file, converts to a pandas dataframe object.
     :param csv_file_path: text path to csv file to read
@@ -50,19 +120,97 @@ def read_csv(csv_file_path):
     return df
 
 
-def get_column(df, col_name):
-    """ Get column of a dataframe"""
+def get_column(df: pd.DataFrame, col_name: str) -> pd.DataFrame:
+    """Get column of a dataframe
+    
+    
+    :param df: source dataframe
+    :param col_name: column in dataframe
+    :return: dataframe of N rows and 1 column
+    
+    >>> df = pd.DataFrame({'A': list(range(0, 10)), 'B': list(range(10,20))})
+    >>> df
+       A   B
+    0  0  10
+    1  1  11
+    2  2  12
+    3  3  13
+    4  4  14
+    5  5  15
+    6  6  16
+    7  7  17
+    8  8  18
+    9  9  19
+    >>> get_column(df, 'A')
+    0    0
+    1    1
+    2    2
+    3    3
+    4    4
+    5    5
+    6    6
+    7    7
+    8    8
+    9    9
+    Name: A, dtype: int64
+    >>> get_column(df, 'B')
+    0    10
+    1    11
+    2    12
+    3    13
+    4    14
+    5    15
+    6    16
+    7    17
+    8    18
+    9    19
+    Name: B, dtype: int64
+    >>> get_column(list(range(10)), 'A')
+    Traceback (most recent call last):
+    ...
+    AssertionError
+    >>> get_column(df, 'C')
+    Traceback (most recent call last):
+    ...
+    AssertionError
+    """
     assert isinstance(df, pd.DataFrame)
+    assert col_name in df.columns
 
     return df[col_name]
 
 
-def filter_dataframe(df, **kwargs):
-    """
+def filter_dataframe(df: pd.DataFrame, **kwargs) -> pd.DataFrame:
+    """Filters dataframe rows by key-value pairs.
+    
     Generalized method to select the desired row(s) and column(s) from the dataframe. TO BE IMPLEMENTED
-    :param df:
-    :param kwargs:
-    :return:
+    :param df: source dataframe
+    :param kwargs: key-value pairs that represent. Values can be a scalar or list of values. 
+    :return: a filtered copy of the data. 
+    
+    >>> df = pd.DataFrame({'A': list(range(0, 10)), 'B': list(range(10,20))})
+    >>> df
+       A   B
+    0  0  10
+    1  1  11
+    2  2  12
+    3  3  13
+    4  4  14
+    5  5  15
+    6  6  16
+    7  7  17
+    8  8  18
+    9  9  19
+    >>> filter_dataframe(df, A=[1, 2, 4, 6, 8])
+       A   B
+    1  1  11
+    2  2  12
+    4  4  14
+    6  6  16
+    8  8  18
+    >>> filter_dataframe(df, B=15)
+       A   B
+    5  5  15
     """
     working_df = df.copy()
 
@@ -71,3 +219,8 @@ def filter_dataframe(df, **kwargs):
         working_df = working_df[working_df[str(key)].isin(value)]
 
     return working_df
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
--- crash_analysis/preprocess.py
+++ crash_analysis/preprocess.py
@@ -1,13 +1,16 @@
 import functools
 import re
-
 import nltk
 from nltk import pos_tag, SnowballStemmer
-
+from typing import List, Sequence, TypeVar, Callable, Optional, Union, Any
 from crash_analysis.dataframe_helper import fill_empty
+import pandas as pd
+
+T = TypeVar('T')
+K = TypeVar('K')
 
 
-def preprocess(df, _map=None):
+def preprocess(df: pd.DataFrame, _map: Optional[Callable[[T], Any]] = None) -> pd.DataFrame:
     """Preprocesses the working dataframe by tokenizing and stemming every word
 
     This function is can be overloaded with any type of "_map", or function to apply to every row of the dataframe. 
@@ -31,7 +34,7 @@ def preprocess(df, _map=None):
     return processed_df
 
 
-def strip_proper_pos(text):
+def strip_proper_pos(text: Union[List[str], str]) -> List[str]:
     """Return list of words as long as they are not proper nouns
     
     Works via part-of-speech (pos) tagging from the natural language tool kit (nltk). 
@@ -51,8 +54,15 @@ def strip_proper_pos(text):
     return without_propernouns
 
 
-def tokenize_and_stop(text):
-    """Lowers, tokenizes input and removes stop words. """
+def tokenize_and_stop(text: Union[List[str], str]) -> List[str]:
+    """Lowers, tokenizes input and removes stop words. 
+    
+    :param text: A list of tokens or a string
+    :return: List of processed tokens
+    
+    >>> tokenize('I Love Coding, Running, And Eating', stem=False)
+    ['love', 'coding', 'running', 'eating']
+    """
 
     text = __join_if_list(text)
 
@@ -65,13 +75,16 @@ def tokenize_and_stop(text):
     return final_tokens
 
 
-def tokenize_stem_stop(text):
+def tokenize_stem_stop(text: Union[List[str], str]):
     """Tokenizes, stems, and removes stop words. 
     
     Function that maps string input to a list of tokens. The token list has no stopwords and all words "stemmed",
     or transformed to their root word.
     :param text: Input string
     :return: list of processed tokens
+    
+    >>> tokenize_stem_stop('I Love Coding, Running, And Eating')
+    ['love', 'code', 'run', 'eat']
     """
     text = __join_if_list(text)
 
@@ -86,15 +99,25 @@ def tokenize_stem_stop(text):
     return stems
 
 
-def tokenize(text, stem=True, stop=True):
+def tokenize(text: Union[List[str], str], stem: bool = True, stop: bool = True) -> List[str]:
     """Generalized tokenization function. 
     
     Function that maps string input to a list of tokens. The token list has no stopwords and all words "stemmed",
     or transformed to their root word.
     :param text: Input string
+    :param stem: should you stem the tokens?
+    :param stop: should you remove stop words?
     :return: list of processed tokens
+    
+    >>> tokenize('I Love Coding, Running, And Eating')
+    ['love', 'code', 'run', 'eat']
+    >>> tokenize('I Love Coding, Running, And Eating', stop=False)
+    ['i', 'love', 'code', 'run', 'and', 'eat']
+    >>> tokenize('I Love Coding, Running, And Eating', stem=False)
+    ['love', 'coding', 'running', 'eating']
     """
     def identity(x): return x
+
     def allow_all(x): return True
 
     text = __join_if_list(text)
@@ -103,20 +126,28 @@ def tokenize(text, stem=True, stop=True):
 
     # exclude stopwords (like "a", "the", "in", etc.)
     stopwords = __get_stopwords()
-    def in_stopwords(x): return x in stopwords
+
+    def not_stopword(x): return not x in stopwords
 
     # stem words (e.g {installing, installed, ...} ==> install)
     stemmer = SnowballStemmer('english')
 
     fin_tokens = __map_and_filter(tokens,
                                   stemmer.stem if stem else identity,
-                                  in_stopwords if stop else allow_all)
+                                  not_stopword if stop else allow_all)
 
     return fin_tokens
 
 
-def lower_and_tokenize(text):
-    """lowers input, converts string to list of tokens (delimited by whitespace)."""
+def lower_and_tokenize(text: str) -> List[str]:
+    """lowers input, converts string to list of tokens (delimited by whitespace).
+    
+    :param text: 
+    :return: List of processed tokens
+    
+    >>> lower_and_tokenize('I Love Coding, Running, And Eating')
+    ['i', 'love', 'coding', 'running', 'and', 'eating']
+    """
 
     # tokenize text, split by non-word characters, i.e. characters not in [a-zA-Z0-9_]
     tokens = re.split(r'\W+', text)
@@ -127,15 +158,18 @@ def lower_and_tokenize(text):
     return lower_tokens
 
 
-def ngram(_input, N, skip=None, delim=' ', skip_delim='_'):
-    """ngram-ify a list of tokens.
+def ngram(_input: List[str], N: int, skip: Optional[int] = None,
+          delim: str = ' ', skip_delim: str = '_') -> List[str]:
+    """
+    ngram-ify a list of tokens.
     
     ngrams, to summarize, are groups of single words and n-tuples of words. 
     
     See https://en.wikipedia.org/wiki/N-gram for reference. 
     
     :param _input: input list of tokens (list of strings or a string)
-    :param N: length of grams (the 'n' in ngrams)
+    :param N:  length of grams (the 'n' in ngrams)
+    :param skip: Number of tokens to skip
     :param delim: Delimiter, must be a string
     :param skip_delim: Delimiter for skipped words
     :return: expanded token list of ngrams
@@ -150,23 +184,27 @@ def ngram(_input, N, skip=None, delim=' ', skip_delim='_'):
     for start in range(max_len):
         for n in range(1, min(N+1, max_len+1)):
             end = start + n
-            if end > len(_input):
+            if end > max_len:
                 break
             ngram_tokens.append(delim.join(_input[start:end]))
 
             if skip:
                 skipped = skip_delim.join(_input[start:end:(skip+1)])
-                if skipped not in ngram_tokens:
-                    ngram_tokens.append(skipped)
+                # if skipped not in ngram_tokens:
+                ngram_tokens.append(skipped)
 
     return ngram_tokens
 
 
-def skipgram(_input, N, skip=None, delim=' ', skip_delim='_'):
+def skipgram(_input: List[str], N: int, skip: Optional[int] = None,
+             delim: str = ' ', skip_delim: str = '_') -> List[str]:
     """A variation on ngrams that allows "skips" between tokens. 
+    
+    Will add all skips from 1 to N. 
     
     :param _input: input list of tokens (list of strings or a string)
     :param N: length of grams (the 'n' in ngrams)
+    :param skip: Number of tokens to skip
     :param delim: Delimiter, must be a string
     :param skip_delim: Delimiter for skipped words
     :return: expanded token list of ngrams
@@ -190,11 +228,10 @@ def skipgram(_input, N, skip=None, delim=' ', skip_delim='_'):
                 for s in range(1, skip+1):
 
                     skipped = skip_delim.join(_input[start:end:s])
-                    if skipped not in ngram_tokens:
-                        ngram_tokens.append(skipped)
+                    # if skipped not in ngram_tokens:
+                    ngram_tokens.append(skipped)
 
     return ngram_tokens
-
 
 
 def __get_stopwords():
@@ -209,16 +246,39 @@ def __get_stopwords():
     return stopwords
 
 
-def __join_if_list(text_or_list):
-    """Takes a string or list of strings, returns a string"""
+def __join_if_list(text_or_list: Union[List[str], str]) -> str:
+    """Takes a string or list of strings, returns a string
+    
+    :param text_or_list: either a string or list of tokens
+    :return: a string
+    
+    >>> __join_if_list(['the', 'cat', 'in', 'the', 'hat'])
+    'the cat in the hat'
+    >>> __join_if_list('the cat in the hat')
+    'the cat in the hat'
+    """
 
     if isinstance(text_or_list, list):
         return ' '.join(text_or_list)
     return text_or_list
 
 
-def __map_and_filter(_input, _map=lambda x: x, _filter=lambda x: True):
-    """Combine map and filter into one step"""
+def __map_and_filter(_input: Sequence[T],
+                     _map: Callable[[T], Any] = lambda x: x,
+                     _filter: Callable[[T], bool] = lambda x: True) -> Sequence[Any]:
+    """Combine map and filter into one step
+    
+    :param _input: list of data
+    :param _map: function to map all elements of data (default: identity)
+    :param _filter: function to filter all elements of data (default, include all)
+    :return: mapped and filtered list of data
+    
+    >>> __map_and_filter(range(1,11))
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    >>> __map_and_filter(range(1,11), lambda m: m**2, lambda f: f % 2 == 0)
+    [4, 16, 36, 64, 100]
+    
+    """
 
     return [_map(x) for x in _input if _filter(x)]
 
@@ -229,5 +289,16 @@ def compose(*functions):
     For example, compose(f, g, h)(x) ==> f(g(h(x))). Works like pipe.
     :param functions: one or more functions as arguments
     :return: composition of functions.
+    
+    >>> compose(lambda x: x + 1, lambda y: y*2, lambda z: z**2)(2)
+    9
+    >>> compose(print, int)("10")
+    10
     """
     return functools.reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
